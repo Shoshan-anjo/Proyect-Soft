43a0209e18ff61b69dd08ad0c968f5cca42794e8
Anghelo Johan Montalvo Molina <aj.montalvo@nacionalasistencia.com.bo>
2025-10-14 23:48:16 -0400

    Primer commit

---
+++ dubai/target/debug/deps/dubai-c01ef8cdfa009eff.d
@@ -0,0 +1,5 @@
+C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\dubai-c01ef8cdfa009eff.d: src\main.rs
+
+C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\libdubai-c01ef8cdfa009eff.rmeta: src\main.rs
+
+src\main.rs:
