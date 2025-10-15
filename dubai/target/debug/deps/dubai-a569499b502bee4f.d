22cb8678c5036e5ac511986bb42f75bfb59e0935
Anghelo Johan Montalvo Molina <aj.montalvo@nacionalasistencia.com.bo>
2025-10-15 01:37:41 -0400

    Creacion de migracion de conexion a db

--- dubai/target/debug/deps/dubai-a569499b502bee4f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\dubai-a569499b502bee4f.d: src\main.rs
-
-C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\libdubai-a569499b502bee4f.rmeta: src\main.rs
-
-src\main.rs:
--- dubai/target/debug/deps/dubai-c01ef8cdfa009eff.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\dubai-c01ef8cdfa009eff.d: src\main.rs
-
-C:\Users\aj.montalvo\Rust\dubai\target\debug\deps\libdubai-c01ef8cdfa009eff.rmeta: src\main.rs
-
-src\main.rs:
